bff38cc597e682eb9d36ad1ff2dcb13f5697e4a8
Aulia153 <auliasilmi133@gmail.com>
2026-06-07 11:06:40 +0700

    workflow ci

---
+++ MLProject/modelling.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+import mlflow
+import mlflow.tensorflow
+
+DATASET_PATH = "mango_preprocessing"
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    DATASET_PATH,
+    validation_split=0.2,
+    subset="training",
+    seed=42,
+    image_size=(224,224),
+    batch_size=32
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    DATASET_PATH,
+    validation_split=0.2,
+    subset="validation",
+    seed=42,
+    image_size=(224,224),
+    batch_size=32
+)
+
+mlflow.tensorflow.autolog()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(224,2244,3)),
+    tf.keras.layers.Rescaling(1./255),
+
+    tf.keras.layers.Conv2D(32,3,activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(64,3,activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Conv2D(128,3,activation='relu'),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.Flatten(),
+
+    tf.keras.layers.Dense(128,activation='relu'),
+
+    tf.keras.layers.Dense(3,activation='softmax')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+with mlflow.start_run():
+
+    model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=10
+    )
+
+    model.save("mango_model.keras")
